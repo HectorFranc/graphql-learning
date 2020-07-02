f6727044376b575f4a2ab45a0377ff70e5600572
Hector Franco T <tor6.franco6@hotmail.com>
2020-07-02 18:45:36 -0500

    Server

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-const { graphql, buildSchema } = require('graphql')
+const { buildSchema } = require('graphql')
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+
+const port = process.env.port || 3000
+const app = express()
 
 // Create schema
 const schema = buildSchema(`
     type Query {
+        "Retorna un saludo"
         hello: String,
-        bye: String
     }
 `)
 
 // Resolvers
 const resolvers = {
     hello: () => "Hello, world",
-    bye: () => "Bye world"
 }
 
-// Execute query
-graphql(schema, '{ hello }', resolvers)
-    .then( data => {
-        console.log(data)
-    } )
-    .catch( errors => {
-        console.error(errors)
-    })
+// Server
+app.use('/api', gqlMiddleware({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true,
+}))
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}/api`)
+})
