733b6b3a914a78f052e2b29d241af43459b7f113
Hector Franco T <tor6.franco6@hotmail.com>
2020-07-02 19:17:33 -0500

    Modularization and custom types

--- index.js
+++ index.js
@@ -2,29 +2,29 @@ const { buildSchema } = require('graphql')
 const express = require('express')
 const gqlMiddleware = require('express-graphql')
 
+const resolvers = require('./lib/resolvers')
+
+const { readFileSync } = require('fs')
+const { join } = require('path')
+
 const port = process.env.port || 3000
 const app = express()
 
 // Create schema
-const schema = buildSchema(`
-    type Query {
-        "Retorna un saludo"
-        hello: String,
-    }
-`)
-
-// Resolvers
-const resolvers = {
-    hello: () => "Hello, world",
-}
+const schema = buildSchema(
+    readFileSync(
+        join(__dirname, 'lib', 'schema.graphql'),
+        'utf-8'
+    )
+)
 
 // Server
 app.use('/api', gqlMiddleware({
-    schema: schema,
-    rootValue: resolvers,
-    graphiql: true,
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
 }))
 
 app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}/api`)
+  console.log(`Server is listening at http://localhost:${port}/api`)
 })
