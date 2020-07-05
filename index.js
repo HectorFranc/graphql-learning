f05db6869748923450b7ef943f29d864979852c7
Hector Franco T <tor6.franco6@hotmail.com>
2020-07-05 10:43:29 -0500

    Production mode

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
+const cors = require('cors')
 const gqlMiddleware = require('express-graphql')
 
 const resolvers = require('./lib/resolvers')
@@ -9,6 +10,7 @@ const { readFileSync } = require('fs')
 const { join } = require('path')
 
 const port = process.env.port || 3000
+const isDev = process.env.NODE_ENV !== 'production'
 const app = express()
 
 // Create schema
@@ -21,11 +23,13 @@ const schema = makeExecutableSchema({
     resolvers
 })
 
+app.use(cors())
+
 // Server
 app.use('/api', gqlMiddleware({
   schema: schema,
   rootValue: resolvers,
-  graphiql: true,
+  graphiql: isDev,
   customFormatErrorFn: errorHandler,
 }))
 
